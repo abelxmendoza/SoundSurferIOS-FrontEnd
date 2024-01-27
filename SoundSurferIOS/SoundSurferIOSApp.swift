301ef5be798d0c562e4bd331a0f08362deca6ced
Abel Mendoza <abelxmendoza@gmail.com>
2024-01-26 22:24:20 -0800

    Initial Commit

---
+++ SoundSurferIOS/SoundSurferIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundSurferIOSApp.swift
+//  SoundSurferIOS
+//
+//  Created by Abel Mendoza on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundSurferIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
